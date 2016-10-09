074f8ded279b678b2e8ebb3247f577cc9c25c4fd
root <reddeveloppement@wanalike.fr>
2016-10-09 16:56:24 +0200

    side bar in progress contains in rest

--- main.php
+++ main.php
@@ -127,31 +127,54 @@
 			</section>
 
 			<section class="col-xs-2  side">
-			<!--sidebar hobbies-->
-			<div class="col-xs-6 col-sm-12 sidebar-offcanvas down" id="sidebar">
-				<div class="list-group">
-					<a href="#" class="list-group-item active">Hobbies</a>
-					<a href="#" class="list-group-item">Link</a>
-					<a href="#" class="list-group-item">Link</a>
-					<a href="#" class="list-group-item">Link</a>
-					<a href="#" class="list-group-item">Link</a>
-
-				</div>
-			</div><!--/span-->
-
 			
-			<div class="col-xs-6 col-sm-12 sidebar" id="sidebar">
-				<div class="list-group">
-					<a href="#" class="list-group-item active">Mes Liens</a>
+			<div class="sidebar-wrapper">
+            
+            
+            
+                <ul class="list-unstyled contact-list">
+                    <li class="email"><i class="fa fa-envelope"></i><a href="mailto: yourname@email.com">alan.doe@website.com</a></li>
+                    <li class="phone"><i class="fa fa-phone"></i><a href="tel:0123 456 789">0123 456 789</a></li>
+                    <li class="website"><i class="fa fa-globe"></i><a href="http://themes.3rdwavemedia.com/website-templates/free-responsive-website-template-for-developers/" target="_blank">portfoliosite.com</a></li>
+                    <li class="linkedin"><i class="fa fa-linkedin"></i><a href="#" target="_blank">linkedin.com/in/alandoe</a></li>
+                    <li class="github"><i class="fa fa-github"></i><a href="#" target="_blank">github.com/username</a></li>
+                    <li class="twitter"><i class="fa fa-twitter"></i><a href="https://twitter.com/3rdwave_themes" target="_blank">@twittername</a></li>
+                </ul>
+            <!--//contact-container-->
+            
+                <h2 class="container-block-title">Education</h2>
+                <div class="item">
+                    <h4 class="degree">MSc in Computer Science</h4>
+                    <h5 class="meta">University of London</h5>
+                    <div class="time">2011 - 2012</div>
+                </div><!--//item-->
+                <div class="item">
+                    <h4 class="degree">BSc in Applied Mathematics</h4>
+                    <h5 class="meta">Bristol University</h5>
+                    <div class="time">2007 - 2011</div>
+                </div><!--//item-->
+            <!--//education-container-->
+            
+            
+                <h2 class="container-block-title">Languages</h2>
+                <ul class="list-unstyled interests-list">
+                    <li>French <span class="lang-desc">(Native)</span></li>
+                    <li>English <span class="lang-desc">(Professional)</span></li>
+                    
+                </ul>
+            <!--//interests-->
+            
+            
+                <h2 class="container-block-title">Interests</h2>
+                <ul class="list-unstyled interests-list">
+                    <li>Climbing</li>
+                    <li>Snowboarding</li>
+                    <li>Cooking</li>
+                </ul>
+            <!--//interests-->
+            
+        <!--//sidebar-wrapper-->
 
-					<a href="#" class="list-group-item">
-					<img class="in" src="img/in.png"/>Julienvaupré</a>
-
-					<a href="#" class="list-group-item coll"><img class="twit" src="img/twit.png"/>JulienV</a>
-
-					<a href="#" class="list-group-item"><img class="git" src="img/git.png "/> JeanSairien</a>
-				</div>
-			</div><!--/span-->				 
 			</section>
 			<section class="col-lg-10">		
 				<div class="col-md-8 ">
