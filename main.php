ea163bdcb345bc11ad164f348c1e2d023ef3dea5
root <reddeveloppement@wanalike.fr>
2016-10-09 15:31:45 +0200

    add skillz bare , solved sidebar

---
+++ main.php
@@ -0,0 +1,203 @@
+
+<!DOCTYPE>
+<html>
+<head>
+
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Developper Web</title>
+	<!--font-->
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet" type="text/css">
+	<link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Faster+One' rel='stylesheet' type='text/css'> 
+	<link href='https://fonts.googleapis.com/css?family=VT323' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,400italic,300italic,300,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+		
+    <!-- Global CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">   
+    <link rel="shortcut icon" href="favicon.ico">  
+    <!-- Theme CSS -->  
+    <link id="theme-style" rel="stylesheet" href="assets/css/styles.css">
+
+	<link rel="stylesheet" type="text/css" href="css/JulienV.css">
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js" integrity="sha384-VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
+  
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.css">
+    
+
+
+</head>
+
+<body>
+	<div class="opac">
+		<div class="container-fluid">
+			
+			<!--contact-->
+			<section class="col-lg-12 bottom">
+			<header>
+				<div class="col-md-12">
+					<div class="col-md-2">
+						<img src="img/me.jpg" class="photo" alt="" />
+
+					</div>
+					<p class="red">Developpeur Passionné</p>
+
+				<article class="skills-section section">
+                <h2 class="section-title"><i class="fa fa-rocket"></i>Skills &amp; Proficiency</h2>
+                <div class="skillset">        
+                    <div class="item">
+                        <h3 class="level-title">PHP &amp; Symfony</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="98%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+                    
+                    <div class="item">
+                        <h3 class="level-title">Javascript &amp; jQuery</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="40%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+                    
+                    <div class="item">
+                        <h3 class="level-title">Angular</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="10%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+                    
+                    <div class="item">
+                        <h3 class="level-title">HTML5 &amp; CSS</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="80%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+                                        
+                    <div class="item">
+                        <h3 class="level-title">Sketch &amp; Photoshop</h3>
+                        <div class="level-bar">
+                            <div class="level-bar-inner" data-level="40%">
+                            </div>                                      
+                        </div><!--//level-bar-->                                 
+                    </div><!--//item-->
+                    
+                </div>  
+            </article><!--//skills-section-->
+				</div>
+				
+			</header>
+				<p class="col-lg-4"> 
+					Vaupré Julien<br />
+					.CACESS1/3/5<br /> 
+					68 RUE Saint-Hovec<br /> 
+					22600 LOUDEAC<br /> 
+					0602074343<br />
+					lesonduko@gmail.com
+				</p>
+
+				<div class="col-md-8 top">	
+
+					<div class="col-md-2">
+						<img src="img/Boostrap.png" class="img1" alt="" />
+					</div>
+
+					<div class="col-md-2">
+						<img src="img/kali.png" class="img2" alt="" />
+					</div>
+
+					<div class="col-md-2">
+						<img src="img/php.png" class="img3" alt="" />
+					</div>
+
+					<div class="col-md-2">
+						<img src="img/mysql.png" class="img4" alt="" />
+					</div>
+				</div> 	
+
+			</section>
+
+			<section class="col-xs-2  side">
+			<!--sidebar hobbies-->
+			<div class="col-xs-6 col-sm-2 sidebar-offcanvas down" id="sidebar">
+				<div class="list-group">
+					<a href="#" class="list-group-item active">Hobbies</a>
+					<a href="#" class="list-group-item">Link</a>
+					<a href="#" class="list-group-item">Link</a>
+					<a href="#" class="list-group-item">Link</a>
+					<a href="#" class="list-group-item">Link</a>
+
+				</div>
+			</div><!--/span-->
+
+			
+			<div class="col-xs-4 col-sm-2 sidebar" id="sidebar">
+				<div class="list-group">
+					<a href="#" class="list-group-item active">Mes Liens</a>
+
+					<a href="#" class="list-group-item">
+					<img class="in" src="img/in.png"/>Julienvaupré</a>
+
+					<a href="#" class="list-group-item coll"><img class="twit" src="img/twit.png"/>JulienV</a>
+
+					<a href="#" class="list-group-item"><img class="git" src="img/git.png "/> JeanSairien</a>
+				</div>
+			</div><!--/span-->				 
+			</section>
+			<section class="col-lg-10">		
+				<div class="col-md-8 ">
+					<img src="img/" /><h3>www.shambhalafm.fr<h3>
+					<h3>Web Radio a theme OpenSource</h3>
+				
+					<img src="" /><h3>www.wanalike.fr<h3>
+					<h3>Site de rencontre et de partage </h3>
+				
+					<img src="" /><h3>www.wanahost.fr </h3>
+					<h3>plateforme d'hebergement associative bretonne</h3>
+				
+					<img src="" /><h3>2tech</h3>
+					<h3>developpement d'un panel de Gestion de stock en php/sql</h3>
+				
+					<img src="" /><h3>Ebulition.fr</h3>
+					<h3>Site Vitrine Vente de vetements</h3>
+				</div>
+				</section>	
+
+
+
+			<section class="col-lg-10">
+
+				<div class="col-md-8">
+					<h2>Formations</h2>
+					<h3>BEP éléctronique
+						2004 Lycée Sully Nogent Le Rotrou(28)</h3>
+
+						<h3>Magasinier Cariste
+							2009 Afpa de Lorient(56) </h3>
+							<h3>Developpeur Web Junior
+								2016 Code Academy Rennes(35)</h3>
+								
+
+							</div>
+
+						</section>
+						<!--footer-->
+						<footer class="col-lg-12">
+						<p class="nintendo">Powered BY J.V</p>
+						</footer>
+					</div>
+
+				</div>
+			</body>
+			
+			</html>
+
